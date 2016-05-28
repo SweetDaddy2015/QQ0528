3fc1d427f2b91bb5e4ee1e69dc4deb0a5c0642ef
我就叫Hank <viphank@sina.com>
2016-05-28 22:31:36 +0800

    初始化项目

---
+++ QQ0528/QQ0528/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QQ0528
+//
+//  Created by H on 16/5/28.
+//  Copyright © 2016年 TanZhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QQ0528/QQ0528/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  QQ0528
+//
+//  Created by H on 16/5/28.
+//  Copyright © 2016年 TanZhou. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
